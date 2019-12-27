43006001c0cedbd25e0ec1049ae78c7bc101e988
zaripovasb <sabina.zaripova@nu.edu.kz>
2019-12-27 19:39:02 +0600

    Find Numbers with Even Number of Digits

---
+++ arrays/EvenDigitNumbers.cpp
@@ -0,0 +1,15 @@
+// O(n) solution
+class Solution {
+public:
+     int findNumbers(vector<int>& nums) {
+        int evenDigits=0;
+    for(int i=0;i<nums.size();i++){
+        string temp=to_string(nums[i]);
+        if(temp.size()%2==0){
+            evenDigits++;
+        }
+
+    }
+    return evenDigits;
+    }
+};
