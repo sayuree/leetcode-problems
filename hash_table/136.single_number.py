48e6540cb1a50a39f392f5034fe8d60b482e60d4
zaripovasb <sabina.zaripova@nu.edu.kz>
2020-06-30 15:10:29 +0600

    Single Number: using python dictionary

---
+++ hash_table/136.single_number.py
@@ -0,0 +1,19 @@
+# Runtime: 112 ms, faster than 29.70% of Python3 online submissions for Single Number.
+# Memory Usage: 16.2 MB, less than 89.78% of Python3 online submissions for Single Number.
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        my_map = {}
+        for item in nums:
+            if item in my_map:
+                my_map[item] += 1
+            else:
+                my_map[item] = 1
+
+        res = 0
+        for key in my_map:
+            if my_map[key] == 1:
+                res = key
+                break
+        return res
