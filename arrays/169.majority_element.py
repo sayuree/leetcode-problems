223ec302c099435018294c9ddb1e78bf8248a8f8
zaripovasb <sabina.zaripova@nu.edu.kz>
2020-07-01 17:17:56 +0600

    Edited majority element using max function on dictionary

--- arrays/169.majority_element.py
+++ arrays/169.majority_element.py
@@ -21,3 +21,22 @@ class Solution:
                 output = key
 
         return output
+# Runtime: 184 ms, faster than 34.48% of Python3 online submissions for Majority Element.
+# Memory Usage: 15 MB, less than 96.94% of Python3 online submissions for Majority Element.
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        my_map = {}
+        output = 0
+
+        if nums == None:
+            return null
+
+        for item in nums:
+            if item in my_map:
+                my_map[item] += 1
+            else:
+                my_map[item] = 1
+
+        return max(my_map, key=my_map.get)
