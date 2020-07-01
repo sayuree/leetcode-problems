746978ff0aeb05f8457ada847023c9eb825081c1
zaripovasb <sabina.zaripova@nu.edu.kz>
2020-07-01 16:44:39 +0600

    Edited single number

--- hash_table/136.single_number.py
+++ hash_table/136.single_number.py
@@ -17,3 +17,19 @@ class Solution:
                 res = key
                 break
         return res
+# Improved code using min() function that can be applied to iterable
+
+# Runtime: 92 ms, faster than 53.40% of Python3 online submissions for Single Number.
+# Memory Usage: 16.2 MB, less than 72.94% of Python3 online submissions for Single Number.
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        my_map = {}
+        for item in nums:
+            if item in my_map:
+                my_map[item] += 1
+            else:
+                my_map[item] = 1
+
+        return min(my_map, key=my_map.get)
