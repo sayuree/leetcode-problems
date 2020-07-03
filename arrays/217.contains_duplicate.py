f6c7e82797a3572226a12a4dae0c50b98e462224
zaripovasb <sabina.zaripova@nu.edu.kz>
2020-07-03 21:39:44 +0600

    Solved Contains Duplicate using set

---
+++ arrays/217.contains_duplicate.py
@@ -0,0 +1,12 @@
+# Runtime: 204 ms, faster than 8.17% of Python3 online submissions for Contains Duplicate.
+# Memory Usage: 19.3 MB, less than 38.17% of Python3 online submissions for Contains Duplicate.
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if not nums:
+            return False
+        my_set = set()
+        for item in nums:
+            my_set.add(item)
+        return len(my_set) != len(nums)
